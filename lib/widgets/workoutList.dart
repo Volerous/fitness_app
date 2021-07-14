0bfb39800f8c26422b2d899a4d19688ff65b4398
Peter Nicolas McDaniel <kg.mcd@hotmail.com>
2021-07-15 00:17:55 +0200

    update

--- lib/widgets/workoutList.dart
+++ lib/widgets/workoutList.dart
@@ -2,7 +2,8 @@ import 'package:fitness_app/repo/workout.dart';
 import 'package:flutter/material.dart';
 
 class WorkoutListView extends StatefulWidget {
-  WorkoutListView({ Key? key, required List<Workout> workouts}) : super(key: key);
+  WorkoutListView({Key? key, required List<Workout> workouts})
+      : super(key: key);
   final List<Workout> workouts = [];
   @override
   _WorkoutListViewState createState() => _WorkoutListViewState();
@@ -11,8 +12,13 @@ class WorkoutListView extends StatefulWidget {
 class _WorkoutListViewState extends State<WorkoutListView> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [Text("test")],
+    return ListView.separated(
+      separatorBuilder: (c, i) => Divider(thickness: 1.0, color: Colors.black),
+      padding: const EdgeInsets.all(3.0),
+      itemBuilder: (context, i) => ListTile(
+        title: Text("$i"),
+      ),
+      itemCount: 3,
     );
   }
 }
